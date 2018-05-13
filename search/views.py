57c0c13f14a0f759e9a1b6bb72f065a2902f93b6
StepanSamsonov <Mynameisericlensherr@gmail.com>
2018-05-13 23:37:12 +0300

    Added working search

--- search/views.py
+++ search/views.py
@@ -3,21 +3,28 @@ import sys
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
+from product.models import Product
 from search.forms import SearchForm
 
 
 def search(request):
-    text = request.session.get('text_search')
-    print(sys.stderr, 'IN SEARCH')
+    text_search = ''
+
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            text_search = form.cleaned_data['text_search']
+
+    else:
+        form = SearchForm()
+
+    products = Product.objects.all()
+    searched = []
+
+    for product in products:
+        if (text_search in product.name or text_search in product.description or text_search in product.characteristic
+           or text_search in product.category.name or text_search in product.price_per_some.name
+           or text_search in str(product.price)):
+            searched.append(product)
+    len_prods = len(searched)
     return render(request, 'search.html', locals())
-
-
-    # kekos = Product.objects.all()
-    # searched = []
-    #
-    # for product in products:
-    #     if (text_search in product.name or text_search in product.description or text_search in product.characteristic
-    #        or text_search in product.category.name or text_search in product.price_per_some.name
-    #        or text_search in str(product.price)):
-    #         searched.append(product)
-    # len_prods = len(searched)
