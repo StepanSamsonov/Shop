bae09f1af65340d1ab2d11f4f315027af36b32fc
StepanSamsonov <Mynameisericlensherr@gmail.com>
2018-05-13 22:46:55 +0300

    Some fixes

--- search/views.py
+++ search/views.py
@@ -1,23 +1,15 @@
-from django.shortcuts import render
+import sys
+
+from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-from product.models import Product
 from search.forms import SearchForm
 
 
-@csrf_exempt
 def search(request):
-    text_search = ''
-    if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            text_search = form.cleaned_data['text_search']
-    else:
-        form = SearchForm()
-
-    searched = []
-
-    return render(request, 'search.html', {'form': form}, locals())
+    text = request.session.get('text_search')
+    print(sys.stderr, 'IN SEARCH')
+    return render(request, 'search.html', locals())
 
 
     # kekos = Product.objects.all()
