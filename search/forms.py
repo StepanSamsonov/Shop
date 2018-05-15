39110e67dfc12313c4e1c2924b67abc411ea4b48
StepanSamsonov <Mynameisericlensherr@gmail.com>
2018-05-15 04:15:55 +0300

    Functional for filters

--- search/forms.py
+++ search/forms.py
@@ -1,5 +1,23 @@
 from django import forms
+from product.models import *
 
 
 class SearchForm(forms.Form):
-    text_search = forms.CharField(max_length=100)
+    text_search = forms.CharField(max_length=100, required=False)
+
+
+class FilterForm(forms.Form):
+    choices_for_sort_by = [('filterRadioUp', 'filterRadioUp'), ('filterRadioDown', 'filterRadioDown')]
+    choices_for_per_some = [('За ' + elem.name, 'За ' + elem.name) for elem in PricePerSome.objects.all()]
+    choices_for_per_some.append(('Все', 'Все'))
+    choices_for_exist = [('Все', 'Все'), ('Есть', 'Есть'), ('Нет', 'Нет')]
+    choices_for_categories = [(elem.name, elem.name) for elem in Category.objects.all()]
+    choices_for_categories.append(('Все', 'Все'))
+
+    filter_sort_by = forms.ChoiceField(choices=choices_for_sort_by, widget=forms.RadioSelect(), required=False, initial='filterRadioDown')
+    filter_price_from = forms.IntegerField(required=False)
+    filter_price_to = forms.IntegerField(required=False)
+    filter_per_some = forms.ChoiceField(choices=choices_for_per_some, required=False, initial='Все')
+    filter_is_exist = forms.ChoiceField(choices=choices_for_exist, required=False, initial='Все')
+    filter_categories = forms.ChoiceField(choices=choices_for_categories, required=False, initial='Все')
+    text_search = forms.CharField(max_length=100, required=False)
--- search/views.py
+++ search/views.py
@@ -1,30 +1,52 @@
+from django.shortcuts import render
+from product.models import Product
+from search.forms import SearchForm, FilterForm
 import sys
 
-from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_exempt
 
-from product.models import Product
-from search.forms import SearchForm
-
-
-def search(request):
+def search(request, filter_categories='Все'):
     text_search = ''
+    filter_price_from = 0
+    filter_price_to = 10**6
+    filter_per_some = 'Все'
+    filter_is_exist = 'Все'
+    filter_sort_by = 'filterRadioDown'
 
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            text_search = form.cleaned_data['text_search']
-
+        if 'search_butt' in request.POST:
+            search_form = SearchForm(request.POST)
+            if search_form.is_valid():
+                text_search = search_form.cleaned_data['text_search']
+            filter_form = FilterForm()
+        elif 'filter_butt' in request.POST:
+            filter_form = FilterForm(request.POST)
+            filter_form.fields['filter_sort_by'].label = 'filterRadioDown'
+            if filter_form.is_valid():
+                filter_sort_by = request.POST.get('filterRadio')
+                filter_per_some = request.POST.get('filter_per_some')
+                filter_price_from = filter_form.cleaned_data['filter_price_from']
+                filter_price_to = filter_form.cleaned_data['filter_price_to']
+                filter_is_exist = request.POST.get('filter_is_exist')
+                filter_categories = request.POST.get('filter_categories')
+                text_search = filter_form.cleaned_data['text_search']
+            search_form = SearchForm()
     else:
-        form = SearchForm()
+        search_form = SearchForm()
+        filter_form = FilterForm()
 
     products = Product.objects.all()
     searched = []
-
     for product in products:
         if (text_search in product.name or text_search in product.description or text_search in product.characteristic
            or text_search in product.category.name or text_search in product.price_per_some.name
            or text_search in str(product.price)):
-            searched.append(product)
+            if filter_price_from <= product.price <= filter_price_to:
+                if product.price_per_some.name == filter_per_some[3:] or filter_per_some == 'Все':
+                    if ((product.is_active and filter_is_exist == 'Есть')
+                       or (not product.is_active and filter_is_exist == 'Нет') or (filter_is_exist == 'Все')):
+                        if product.category.name == filter_categories or filter_categories == 'Все':
+                            searched.append(product)
+
+    searched.sort(key=lambda elem: elem.price, reverse=(filter_sort_by == 'filterRadioDown'))
     len_prods = len(searched)
     return render(request, 'search.html', locals())
