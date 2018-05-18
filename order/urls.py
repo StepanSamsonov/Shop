2c748a8071a958f6fd5b3fb6cf8dd5b416282179
StepanSamsonov <Mynameisericlensherr@gmail.com>
2018-05-18 17:33:08 +0300

    Added view and template for order

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'order/', views.order, name='order')
+    url(r'order/(?P<user_name>.*)/$', views.order, name='order')
 ]
--- order/views.py
+++ order/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from product.models import *
+from signup.models import UserAddedData
+from product.models import Product
 
 
-def order(request, order_data=''):
+def order(request, user_name):
     is_login = request.user.is_authenticated()
     user_name = request.user.username
 
+    # data = UserAddedData.objects.get(owner_name=user_name).order_data
+    data = '6:2 7:1 8:5 9:1'
+    data = data.split(' ')
+    data_d = dict()
+    for elem in data:
+        id_p, count = map(int, elem.split(':'))
+        data_d[id_p] = count
+
+    new_data = []
+    order_dict = dict()
+    for id_prod in data_d:
+        try:
+            order_dict[Product.objects.get(id=id_prod)] = data_d[id_prod]
+            new_data.append(str(id_prod) + ':' + str(data_d[id_prod]))
+        except:
+            pass
+    ids = ' '.join(new_data)
     return render(request, 'order.html', locals())
